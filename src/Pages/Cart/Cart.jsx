c6d95bafc9fdbcd2324c6c46eb90ff87a09bb3c5
lami chache <lamichache@gmail.com>
2025-12-01 08:20:08 +0300

    Cart component display basket items

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import LayOut from '../../Components/LayOut/LayOut'
+import { DataContext } from '../../Components/DataProvider/DataProvider'
+import ProductCard from '../../Components/Product/ProductCard'
 
 function Cart() {
+  const [{ basket, user }, dispatch] = useContext(DataContext)
+
   return (
-    
-         <LayOut>
-             <div>Cart</div>
-         </LayOut>
-   
+    <LayOut>
+      <section>
+
+        <div>
+          <h2>Hello</h2>
+          <h3>Your shopping basket</h3>
+          <hr />
+
+          {basket?.length === 0 ? (
+            <p>Your basket is empty</p>
+          ) : (
+            basket.map((item, i) => (
+              <ProductCard
+                key={i}
+                product={item}
+                renderDesc={true}
+                flex={true}
+              />
+            ))
+          )}
+        </div>
+
+        <div>
+          {/* Another section if needed */}
+        </div>
+
+      </section>
+    </LayOut>
   )
 }
 
