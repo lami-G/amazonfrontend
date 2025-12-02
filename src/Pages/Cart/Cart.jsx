9c219c9e32fb082b8d212e6074b39bfb18c39d82
lami chache <lamichache@gmail.com>
2025-12-02 11:25:24 +0300

    Enhance Cart functionality with increment and decrement actions; update reducer logic for basket management

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -5,10 +5,25 @@ import ProductCard from '../../Components/Product/ProductCard'
 import Currencyformat from '../../Components/Currencyformat/Currencyformat'
 import {Link} from 'react-router-dom'
 import classes from './Cart.module.css'
+import {Type} from '../../Utility/actiontype'
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { IoIosArrowUp } from "react-icons/io";
+import { RiArrowUpDoubleLine } from "react-icons/ri";
+import { RiArrowDownDoubleLine } from "react-icons/ri";
 function Cart() {
   const [{ basket, user }, dispatch] = useContext(DataContext)
- const total = basket?.reduce((amount, item) =>{
-  return  item.price + amount },0)
+ const total = basket.reduce((amount, item) =>{
+  return  item.price*item.amount + amount },0)
+  const increment=(item)=>{
+    dispatch({
+      type:Type.ADD_TO_BASKET,item })}
+
+      const decrement=(id)=>{
+        dispatch({
+          type:Type.REMOVE_FROM_BASKET,id })}
+
+
+  console.log(basket)
   return (
     <LayOut>
       <section className={classes.container}>
@@ -18,19 +33,30 @@ function Cart() {
           <h3>Your shopping basket</h3>
           <hr />
 
-          {basket?.length === 0 ? (
+          {
+          basket?.length === 0? (
             <p>Your basket is empty</p>
           ) : (
-            basket.map((item, i) => (
-              <ProductCard
+            basket?.map((item, i) => {
+           return  <section className={classes.cart_product} >
+<ProductCard
                 key={i}
                 product={item}
                 renderDesc={true}
                 renderAdd={false}
                 flex={true}
               />
-            ))
-          )}
+              <div className={classes.btn_container}>
+<button className={classes.btn} onClick={()=>increment(item)}><RiArrowUpDoubleLine size={20}/></button>
+<span>{item.amount}</span>  
+<button  className={classes.btn} onClick={()=>decrement(item.id)}><RiArrowDownDoubleLine size={20} /></button>
+
+              </div>
+
+           </section> 
+})
+          )
+        }
         </div>
 
         
