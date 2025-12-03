d16d7ac6a1f2657b218c95fae7d064a9670067c0
lami chache <lamichache@gmail.com>
2025-12-03 16:28:49 +0300

    loading indicators for sign-in and sign-up;

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -6,31 +6,46 @@ import {auth} from "../../Utility/firebase"
 import { DataContext } from '../../Components/DataProvider/DataProvider'
 import{signInWithEmailAndPassword,createUserWithEmailAndPassword} from "firebase/auth"
 import { Type } from '../../Utility/actiontype'
+import {ClipLoader} from "react-spinners"
 function Auth() {
   const [email,setEmail]=useState("")
   const [password,setPassword]=useState("")
   const [error,setError]=useState(null)
+  const [loading,setLoading]=useState({signIn:false
+  ,signUp:false})
+
 const [{user},dispatch]=useContext(DataContext)
   console.log(user)
   const authHandler=(e)=>{
 e.preventDefault()
 console.log(e.target.name)
 if(e.target.name==="signin"){
+  setLoading({...loading,signIn:true})
   signInWithEmailAndPassword(auth,email,password).then((userinfo)=>{
     console.log(userinfo);
     dispatch({type:Type.SET_USER,user:userinfo.user})
+    setLoading({...loading,signIn:false})
   }).catch((error)=>{
-    console.log(error)
+  
+
+setError(error.message)
+
+setLoading({...loading,signIn:false})
+
   })
 
 }
 else{
+  setLoading({...loading,signUp:true})
   createUserWithEmailAndPassword(auth,email,password).then((userinfo)=>{
-    console.log(userinfo)
+
+   
     dispatch({type:Type.SET_USER,user:userinfo.user})
+    setLoading({...loading,signUp:false}) 
   })
   .catch((error)=>{
-    console.log(error)
+setError(error.message)
+setLoading({...loading,signUp:false})
   })
 }}
   // console.log(email,password)
@@ -66,7 +81,9 @@ type="email" id='email'/>
 
       <button className={ClassNames.login_signinbutton} type='submit' onClick={authHandler}
       name='signin'>
-        Sign in
+
+        {loading.signIn ? <ClipLoader size={20} color='white'/> : " Sign in"}
+     
       </button>
 
 </form>
@@ -75,15 +92,18 @@ type="email" id='email'/>
 
     <button type="submit" onClick={authHandler} className={ClassNames.login_registerbutton}
     name="signup">
-             Create your Amazon Account
+      {loading.signUp?<ClipLoader size={20} color='white'/>:"  Create your Amazon Account"}
+            
     </button>
+
+    {error && <p style={{color:"red",paddingTop:"5px"}}>{error}</p>}
 </div>
 
     </div>
             </section>
         
          
-  )
+  )  
 }
 
 
