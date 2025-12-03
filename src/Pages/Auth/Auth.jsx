29f0070408021789582745855143ff805d97dbc3
lami chache <lamichache@gmail.com>
2025-12-03 07:16:54 +0300

    Implement Auth component with login form and styling;

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -13,12 +13,38 @@ function Auth() {
 
             <Link >
             
-            <img sizes='2'   src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="logo" />
+            <img   src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="logo" />
             </Link>
           </div>
-    <div>
+    <div className={ClassNames.login_container}>
+      <h1>Sign in</h1>
+<form action="" method="post">
 
-      
+
+      <div>
+<label htmlFor="Email"> Email</label>
+<input type="email" id='email'/>
+
+
+      </div>
+      <div>
+        <label htmlFor="password">password</label>
+        <input type="password" id='password' />
+      </div>
+
+      <button className={ClassNames.login_signinbutton} type='submit'>
+        Sign in
+      </button>
+
+</form>
+<div>
+    <p>By signing-in you agree to AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.</p>
+
+    <button className={ClassNames.login_registerbutton}>
+             Create your Amazon Account
+    </button>
+</div>
+
     </div>
             </section>
         
