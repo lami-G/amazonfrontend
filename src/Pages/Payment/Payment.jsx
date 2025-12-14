74e2d87ddb5b49fafee686a046b76e9e10a78e4f
lami chache <lamichache@gmail.com>
2025-12-14 22:20:26 +0300

    Add payment header and basic layout structure to Payment component
    -

--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import LayOut from '../../Components/LayOut/LayOut'
-
+import classes from './Payment.module.css'
 function Payment() {
   return ( <LayOut>
-            <div>Payment</div>
+    {/*header */}
+            <div className={classes.payment_header}> Checkout(2) items</div>
+{/*            payment method */}
+
+<section> 
+{/* address*/}
+<div></div>
+<hr />
+{/* product */}
+<div> </div>
+<hr />
+{/* card*/}
+<div> </div>
+</section>
+
+
+
+
+
          </LayOut>
     
   )
