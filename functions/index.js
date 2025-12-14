6157374843427a8429a82041889ebf7cd2f0948f
lami chache <lamichache@gmail.com>
2025-12-14 17:44:37 +0300

    Refactor code structure for improved readability and maintainability

--- functions/index.js
+++ functions/index.js
@@ -7,26 +7,44 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {setGlobalOptions} = require("firebase-functions");
+
 const {onRequest} = require("firebase-functions/https");
 const logger = require("firebase-functions/logger");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const stripe = require("stripe")( process.env.STRIPE_SECRET_KEY);
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+const app=express();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+app.use(cors({origin:true}));
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.use(express.json());
+app.get("/",(req,res)=>{
+res.status(200).json({message:"sucess"})
+
+})
+app.post("/payments/create",async(req,res)=>{
+const total=req.query.total;
+
+if (total>0){
+    const paymentIntent=await stripe.paymentIntents.create({
+
+        amount:total,
+        currency:"usd",
+    })
+    res.status(201).json({
+
+        clientSecret:paymentIntent.client_secret,
+    })
+    }
+   else{
+    res.status(400).json({message:"invalid amount"})
+   } 
+}
+)
+
+
+
+exports.api=onRequest(app);
