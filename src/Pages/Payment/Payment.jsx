4ec65d3466d13de70c9b1a87d3f2befe93bb6b72
lami chache <lamichache@gmail.com>
2025-12-16 05:53:01 +0300

    Enhance Payment component

--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -1,21 +1,64 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import LayOut from '../../Components/LayOut/LayOut'
 import classes from './Payment.module.css'
+import { DataContext } from '../../Components/DataProvider/DataProvider';
+import ProductCard from '../../Components/Product/ProductCard';
 function Payment() {
+const [{user,basket}, dispatch] = useContext(DataContext);
+console.log(user)
+ const totalItem=basket?.reduce((amount,item)=>{
+    return item.amount + amount
+  },0)
+
+
   return ( <LayOut>
     {/*header */}
-            <div className={classes.payment_header}> Checkout(2) items</div>
+            <div className={classes.payment_header}> Checkout({totalItem}) items</div>
 {/*            payment method */}
 
-<section> 
+<section className={classes.payment} > 
 {/* address*/}
-<div></div>
+<div className={classes.flex}>
+<h3> delivery address</h3>
+
+<div>
+<div>
+{user.email}
+  
+</div>
+<div>={true}
+
+123
+
+</div>
+<div>
+
+
+chicago, IL
+</div>
+</div>
+
+
+
+</div>
+
 <hr />
 {/* product */}
-<div> </div>
+<div className={classes.flex}> 
+<h3> review items and delivery</h3>
+<div>  
+{  basket.map((item)=><ProductCard product={item} flex/>)}
+
+
+</div>
+
+</div>
 <hr />
 {/* card*/}
-<div> </div>
+<div className={classes.flex}>
+  <h3> payment method</h3>
+  
+   </div>
 </section>
 
 
