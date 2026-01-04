e26517ef3591273fa87acb30fabbb66482bca5f2
lami chache <lamichache@gmail.com>
2026-01-04 17:27:34 +0300

    order page

--- src/Pages/Orders/Orders.jsx
+++ src/Pages/Orders/Orders.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
+import React ,{useContext,useState, useEffect}from 'react'
 import LayOut from '../../Components/LayOut/LayOut'
-
+import classes from "./Orders.module.css"
+import { db } from '../../Utility/firebase'
+import { DataContext } from '../../Components/DataProvider/DataProvider'
 function Orders() {
+
+
+  const [{user},dispatch]=useContext(DataContext);
+  const [orders,setorder]=useState([])
+  useEffect(()=>{
+
+
+
+
+    if(user){
+db.collection("users").doc(user.uid).collection("orders")
+
+
+
+    }
+  })
   return (
 
      <LayOut>
-         <div>Orders</div>   
+       <section className={classes.container}>
+        <div className={classes.order_container}>
+
+
+<h2>  your orders</h2>
+
+{/*   ordered items */}
+<div>                 </div>
+
+
+
+        </div>
+        
+        
+        </section> 
          </LayOut>
     
   )
 }
-
+ 
 export default Orders
